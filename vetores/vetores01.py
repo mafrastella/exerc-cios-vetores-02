669f00eedc96118ead57e9974419b349ec13c6a4
Stella e Sabina <ribeirostellaribeiro@gmail.com>
2024-08-30 18:19:29 -0300

    exercício de vetores 02

---
+++ vetores/vetores01.py
@@ -0,0 +1,9 @@
+# Faça um programa onde lê os números que o usuário digitar.
+
+número = int(input("Digite 10 números aletórios: "))
+número = [
+    print(int(input("números_digitados")))
+]
+
+# exibir os números digitados pelo usuário
+número.reverse()
